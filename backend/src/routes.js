3a02b7e9c736ffc20df5a40c5a61ab95affb3b5e
lucas <lucas.san.77@gmail.com>
2020-02-15 12:57:47 -0300

    commit 3

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
-  const frase = 'Hello palpiterio';
+  const frase = 'Hello palpiteiro';
   return res.send(frase);
 });
 
