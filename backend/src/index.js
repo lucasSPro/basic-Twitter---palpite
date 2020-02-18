43021a4fe42aa6eb776e575ce054e31acd5d3ac1
lucas <lucas.san.77@gmail.com>
2020-02-17 22:40:14 -0300

    commit 5

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,15 +2,18 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const routes = require('./routes');
+const env = require('./env');
 
 const app = express();
 
 mongoose.connect(
-  'mongodb+srv://goweek:goweek@cluster0-ikt19.mongodb.net/test?retryWrites=true&w=majority', {
+  `mongodb+srv://goweek:${env.password}@cluster0-ikt19.mongodb.net/test?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   },
 );
+
+// mongodb+srv://goweek:<password>@cluster0-ikt19.mongodb.net/test?retryWrites=true&w=majority
 
 app.use(express.json());
 app.use(routes);
