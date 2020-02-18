46d980152e82dce5b2b03ce1900361d399118bfa
lucas <lucas.san.77@gmail.com>
2020-02-18 14:38:42 -0300

    initial frontend Web

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' component={}/>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
